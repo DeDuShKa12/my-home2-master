c1cbb4590154d274193942e167847ddb30ad33f2
DeDuShKa12 <pasharudiuk97@gmail.com>
2024-01-03 12:51:05 +0200

    steps animation done?

--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -1,86 +1,3 @@
-// import React, {FC, useEffect, useState} from 'react';
-// import './Steps.css';
-//
-// const Steps: FC = () => {
-//     const [isVisible, setIsVisible] = useState(false);
-//     const [animationCompleted, setAnimationCompleted] = useState(false);
-//
-//     useEffect(() => {
-//         function handleScroll() {
-//             const targetPosition = window.innerHeight * 0.30;
-//
-//             if (window.scrollY >= targetPosition) {
-//                 setIsVisible(true);
-//                 window.removeEventListener('scroll', handleScroll);
-//             }
-//         }
-//
-//         window.addEventListener('scroll', handleScroll);
-//
-//         return () => {
-//             window.removeEventListener('scroll', handleScroll);
-//         };
-//     }, []);
-//
-//     useEffect(() => {
-//         if (isVisible && !animationCompleted) {
-//             const steps = document.querySelectorAll<HTMLElement>('.step');
-//
-//             steps.forEach((step, index) => {
-//                 setTimeout(() => {
-//                     if (index % 2 !== 0) {
-//                         step.classList.add('visible1');
-//                     } else {
-//                         step.classList.add('visible');
-//                     }
-//                     console.log(index);
-//                 }, index * 500);
-//             });
-//
-//             setAnimationCompleted(true); // Позначте, що анімація вже виконана
-//         }
-//     }, [isVisible, animationCompleted]);
-//
-//     return (
-//         <div className={`steps-container ${isVisible ? 'visible' : ''}`}>
-//             <div className="step">
-//                 <div className="numberAndLogoDiv">
-//                     <div className="numberDiv">Step 1</div>
-//                     <img src="/logoSolo.png" alt="Logo"/>
-//                 </div>
-//                 <h1>Fill Out A Short Questionnaire</h1>
-//                 <div>
-//                     Provide answers to just 5 questions so that we can better understand
-//                     your situation and get in touch with you.
-//                 </div>
-//             </div>
-//             <div className="step">
-//                 <div className="numberAndLogoDiv">
-//                     <div className="numberDiv">Step 2</div>
-//                     <img src="/logoSolo.png" alt="Logo"/>
-//                 </div>
-//                 <h1>Property Analysis & No Obligation Offer</h1>
-//                 <div>
-//                     We will reach out to you and offer an analysis of your options, as well as a cash offer "as is."
-//                     No real estate agents needed!
-//                 </div>
-//             </div>
-//             <div className="step">
-//                 <div className="numberAndLogoDiv">
-//                     <div className="numberDiv">Step 3</div>
-//                         <img src="/logoSolo.png" alt="Logo"/>
-//                 </div>
-//                 <h1>Close Deal & Cash Payment</h1>
-//                 <div>
-//                     We offer cash for your home and can close the deal in as little as 3 days. We'll deposit $10,000
-//                     earnest money and cover all closing costs.
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export {Steps};
 import React, { FC, useEffect, useState } from 'react';
 import './Steps.css';
 
@@ -105,7 +22,7 @@ const Steps: FC = () => {
 
     useEffect(() => {
         function handleScroll() {
-            const targetPosition = window.innerHeight * 0.30;
+            const targetPosition = window.innerHeight * 0.15;
 
             if (window.scrollY >= targetPosition) {
                 setIsVisible(true);
@@ -114,8 +31,7 @@ const Steps: FC = () => {
             }
         }
 
-        // Визначте, чи користувач вже прокрутив сторінку при завантаженні
-        const isUserAlreadyScrolled = window.scrollY >= window.innerHeight * 0.30;
+        const isUserAlreadyScrolled = window.scrollY >= window.innerHeight * 0.15;
         if (isUserAlreadyScrolled) {
             setIsVisible(true);
             startAnimation();
